a193d9595a5654d0ee3812954799d20c70df052e
ijeny <imtinan.jmb6628@gmail.com>
2025-10-22 09:44:25 +0700

    Add files via upload

---
+++ tugas 4/fungsi.js
@@ -0,0 +1,38 @@
+var dataBarang = [
+    "Buku Tulis",
+    "Pensil",
+    "Spidol"
+];
+
+function showBarang(){
+    var listbarang = document.getElementById("list-barang");
+    //clear list barang
+    listbarang.innerHTML = "";
+
+    //cetak semua barang
+    for(let i = 0; i < dataBarang.length; i++){
+        var btnEdit = "<a href='#' onclick='editBarang("+i+")'>Edit</a>";
+        var btnHapus = "<a href='#' onclick='deleteBarang("+i+")'>Hapus</a>";
+
+        listbarang.innerHTML += "<li>" + dataBarang[i] + "["+btnEdit +" | "+ btnHapus +"]</li>";
+    }
+}
+
+function addBarang(){
+    var input = document.querySelector("input[name=barang]");
+    dataBarang.push(input.value);
+    showBarang();
+}
+
+function editBarang(id){
+    var newBarang = prompt("Nama baru", dataBarang[id]);
+    dataBarang[id] = newBarang;
+    showBarang();
+}
+
+function deleteBarang(id){
+    dataBarang.splice(id, 1);
+    showBarang();
+}
+
+showBarang();
